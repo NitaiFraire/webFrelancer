9c015262d398cd69fcb99a3980107f8dd301c065
NitaiFraire <fernandezfrairenitai@hotmail.com>
2018-11-30 20:31:12 -0600

    finalizacion de proyecto

---
+++ js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    cargarPortafolio();
+});
+
+
+
+function cargarPortafolio() {
+    fetch('datos.json')
+        .then((respuesta) => {
+            return respuesta.json();
+        })
+        .then((datos) => {
+            let html = '';
+
+            datos.portafolio.forEach(portafolio => {
+                // Crear template
+                html += `
+                    <div class="elemento">
+                        <img src="img/${portafolio.id}.jpg">
+                        <div class="contenido">
+                            <h3>${portafolio.nombre}</h3>
+                            <p>${portafolio.desc}</p>
+                        </div>
+                    </div>
+                `;
+            });
+
+            //despues de crear template inyectar al html
+            document.querySelector('#listado').innerHTML = html;
+        })
+}
